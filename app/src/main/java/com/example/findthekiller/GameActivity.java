8c0b0f1d37d4c7b350cd7a139a09106978dabac9
dreath <75094352+dreath-pro@users.noreply.github.com>
2024-03-05 19:01:47 +0800

    improved main game layout

--- app/src/main/java/com/example/findthekiller/GameActivity.java
+++ app/src/main/java/com/example/findthekiller/GameActivity.java
@@ -14,11 +14,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
     TextView chatBox;
 
     SpannableStringBuilder builder = new SpannableStringBuilder();
+    ArrayList<PlayerModel> playerModels = new ArrayList<>();
+    ArrayList<Characters> character = new ArrayList<>();
+    RecyclerView playerInterrogation;
+    Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +36,23 @@ public class GameActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_game);
 
+        playerInterrogation = findViewById(R.id.playerInterrogation);
         chatBox = findViewById(R.id.chatBox);
 
         builderMessages();
         chatBox.setText(builder);
+
+        addCharacter();
+        for (int i = 0; i <= 7; i++) {
+            int characterSelection = random.nextInt(character.size());
+            playerModels.add(new PlayerModel(character.get(characterSelection).getName(), character.get(characterSelection).getGender(), null,
+                    character.get(characterSelection).getCloseView(), character.get(characterSelection).getDeadImage(), character.get(characterSelection).getCloseView()));
+        }
+
+        InterrogationAdapter interrogationAdapter = new InterrogationAdapter(this, playerModels);
+        playerInterrogation.setAdapter(interrogationAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        playerInterrogation.setLayoutManager(layoutManager);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.gameLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -47,5 +70,16 @@ public class GameActivity extends AppCompatActivity {
 
         builder.append("Kiwi: Hey haha you are cute muah! wanna play some other time caleb?");
         builder.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_NORMAL), builder.length() - 9, builder.length(), 0);
+    }
+
+    private void addCharacter() {
+        character.add(new Characters(R.drawable.player, R.drawable.player, "Dreath", "male"));
+        character.add(new Characters(R.drawable.player, R.drawable.player, "Liam", "male"));
+        character.add(new Characters(R.drawable.player, R.drawable.player, "Kiwi", "female"));
+        character.add(new Characters(R.drawable.player, R.drawable.player, "Jade", "female"));
+        character.add(new Characters(R.drawable.player, R.drawable.player, "Caleb", "male"));
+        character.add(new Characters(R.drawable.player, R.drawable.player, "John", "male"));
+        character.add(new Characters(R.drawable.player, R.drawable.player, "Princess", "female"));
+        character.add(new Characters(R.drawable.player, R.drawable.player, "Kaz", "female"));
     }
 }
---
+++ app/src/main/java/com/example/findthekiller/InterrogationAdapter.java
@@ -0,0 +1,58 @@
+package com.example.findthekiller;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class InterrogationAdapter extends RecyclerView.Adapter<InterrogationAdapter.MyViewHolder>
+{
+    Context context;
+    ArrayList<PlayerModel> playerModels;
+
+    public InterrogationAdapter(Context context, ArrayList<PlayerModel> playerModels)
+    {
+        this.context = context;
+        this.playerModels = playerModels;
+    }
+
+    @NonNull
+    @Override
+    public InterrogationAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.player_selection, parent, false);
+        return new InterrogationAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InterrogationAdapter.MyViewHolder holder, int position) {
+        holder.name.setText(playerModels.get(position).getName());
+        holder.image.setImageResource(playerModels.get(position).getStandImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return playerModels.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder
+    {
+        ImageView image;
+        TextView name;
+
+        public MyViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.playerImage);
+            name = itemView.findViewById(R.id.playerName);
+        }
+    }
+}
